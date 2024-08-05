9efe7363a6c0b736833d0afa666235f7e27424fa
Dharun kumar <dharun.official.01@gmail.com>
2024-08-05 23:10:03 +0530

    Update yolo_node.cpp to incorporate specific use case

--- src/yolo_ros/src/yolo_node.cpp
+++ src/yolo_ros/src/yolo_node.cpp
@@ -2,19 +2,25 @@
 #include "sensor_msgs/msg/image.hpp"
 #include "cv_bridge/cv_bridge.h"
 #include "darknet.h"
+#include <fstream>
+#include <geometry_msgs/msg/pose_stamped.hpp>
 
 class YOLONode : public rclcpp::Node
 {
 public:
-    YOLONode() : Node("yolo_node")
+    YOLONode() : Node("yolo_node"), file_("detected_fruits.txt")
     {
         image_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
             "/camera/image_raw", 10,
             std::bind(&YOLONode::image_callback, this, std::placeholders::_1));
 
+        pose_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
+            "/slam/pose", 10,
+            std::bind(&YOLONode::pose_callback, this, std::placeholders::_1));
+
         // Initialize YOLO
-        net_ = load_network("path_to_cfg_file", "path_to_weights_file", 0);
-        meta_ = get_metadata("path_to_data_file");
+        net_ = load_network("/path/to/yolov3.cfg", "/path/to/yolov3.weights", 0);
+        meta_ = get_metadata("/path/to/coco.data");
     }
 
 private:
@@ -36,12 +42,37 @@ private:
         network_predict_image(net_, im);
         detection *dets = get_network_boxes(net_, im.w, im.h, 0.5, 0.5, 0, 1, NULL, 0, NULL);
         draw_detections(cv_ptr->image, dets, meta_.classes, 0.5);
+        
+        for (int i = 0; i < dets->total; ++i)
+        {
+            if (dets[i].prob[0] > 0.5)
+            {
+                std::string label = meta_.names[dets[i].prob[0]];
+                if (label == "fruit") // Adjust label as per your data file
+                {
+                    file_ << "Fruit detected at: "
+                          << "x: " << current_pose_.pose.position.x
+                          << " y: " << current_pose_.pose.position.y
+                          << " z: " << current_pose_.pose.position.z
+                          << std::endl;
+                }
+            }
+        }
+
         free_detections(dets, meta_.classes);
     }
 
+    void pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
+    {
+        current_pose_ = *msg;
+    }
+
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
+    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub_;
     network *net_;
     metadata meta_;
+    geometry_msgs::msg::PoseStamped current_pose_;
+    std::ofstream file_;
 };
 
 int main(int argc, char *argv[])
